b165138807d2811989ac14da5572edaf8ffda244
zümray Dağaşan <dagasanzumra@gmail.com>
2025-05-17 17:20:44 +0300

    EagleEye-chatbot

---
+++ README.md
@@ -0,0 +1,12 @@
+# 💬 Mini Python Chatbot
+
+A basic rule-based chatbot built using Python. Perfect for beginners to understand how conditional responses and string matching works.
+
+## ✅ Features
+
+👉 Replies to common phrases like: "hi", "hello", "how are you", "bye", etc.
+👉 Easy to expand with more phrases
+👉 No external libraries required
+
+▶️ How to Run
+python chatbot.py
---
+++ chatbot.py
@@ -0,0 +1,37 @@
+from sentence_transformers import SentenceTransformer, util
+import json
+
+model = SentenceTransformer('sentence-transformers/distiluse-base-multilingual-cased-v2')
+
+with open("veri.json", "r", encoding="utf-8") as f:
+    data_list = json.load(f)
+
+faq_data = {item['soru'].lower().strip(): item['cevap'] for item in data_list}
+
+faq_embeddings = model.encode(list(faq_data.keys()), convert_to_tensor=True)
+
+def chatbot_response(user_input):
+    user_input = user_input.lower().strip()
+    input_embedding = model.encode(user_input, convert_to_tensor=True)
+
+    hits = util.semantic_search(input_embedding, faq_embeddings, top_k=1)
+    hit = hits[0][0]
+
+    if hit['score'] > 0.7:
+        matched_question = list(faq_data.keys())[hit['corpus_id']]
+        return faq_data[matched_question]
+    else:
+        return "Üzgünüm, bunu anlayamadım. Başka bir soru sorabilir misiniz?"
+
+def main():
+    print("Siber Güvenlik Chatbot'a Hoşgeldiniz! Çıkmak için 'exit' yazınız.")
+    while True:
+        user_input = input("Sen: ")
+        if user_input.lower().strip() == "exit":
+            print("Chatbot: Görüşürüz!")
+            break
+        response = chatbot_response(user_input)
+        print(f"Chatbot: {response}")
+
+if __name__ == "__main__":
+    main()
