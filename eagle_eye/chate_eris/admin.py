ec0ed9132892d5858793504a54ae05f5bb814a92
melikekaya1001 <mai.portal001@gmail.com>
2025-06-02 12:07:03 +0300

    İlk proje commit'i

---
+++ eagle_eye/chate_eris/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from.models import giris_mesaji
+# Register your models here.
+
+admin.site.register(giris_mesaji)
---
+++ eagle_eye/tarama/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TaramaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tarama'
