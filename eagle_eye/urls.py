408c688c7e33150ad9283d190f6425806a0a8738
Elif Rafa <isbilirelif1903@gmail.com>
2025-06-28 00:13:11 +0300

    Update urls.py

--- eagle_eye/urls.py
+++ eagle_eye/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from chat import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("chat.urls")),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("chat/", views.chat, name="chat"),
 ]
